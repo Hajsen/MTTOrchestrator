ec3972080995c6a7d93cbdf9091ddbfe2097ccf1
Hajsen <Wilmok-2@student.ltu.se>
2017-04-29 16:38:36 +0400

    can rcv and send commands to admin and mt

---
+++ orchestrator.h
@@ -0,0 +1,68 @@
+#ifndef ORCHESTRATOR_H_
+#define ORCHESTRATOR_H_
+
+#include <Dhcp.h>
+#include <Dns.h>
+#include <Ethernet2.h>
+#include <EthernetClient.h>
+#include <EthernetServer.h>
+#include <EthernetUdp2.h>
+#include <Twitter.h>
+#include <util.h>
+
+#include <SPI.h>
+
+#define EOF 3 //end of function
+#define EOT 4 //end of transmission
+#define SOR 5 //start of result
+#define EOR 6 //end of result
+#define WFA 7 //wait for answer
+
+bool debug = 1;
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192, 168, 1, 177);
+IPAddress serverMT(10,160,238,15); 
+// Setup server with (port)
+EthernetServer server(80);
+EthernetClient clientAdmin;
+EthernetClient clientMT;
+
+//Ethernet
+int rcvdpayloadlen = 0;
+char rcvdpayload[255];
+byte nextbyte;
+
+boolean clientConnected = false;
+int ConnectionCheck = 0;
+
+bool sanitizepayload(int nextbyte);
+void handlepayload(byte *payload, int payload_len);
+bool connectToMT();
+void disconnectFromMT();
+bool execMTFunctionCall(char *functionCall, size_t len);
+
+
+#include <mcp_can.h>
+#include <mcp_can_dfs.h>
+
+// CAN0 INT and CS
+#define CAN0_INT 2                              // Set INT to pin 2
+
+MCP_CAN CAN0(11);                               // Set CS to pin 11
+
+// CAN RX Variables
+long unsigned int rxId;
+unsigned char len;
+unsigned char rxBuf[8];
+
+// Serial Output String Buffer
+char msgString[128];
+size_t msgString_len;
+byte msg[] = {0x01, 0x04, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0xF1, 0xF2, 0xF3};
+
+int rcvCan();
+bool sndCan(byte *msg, int msg_len, int dest_id);
+void canInit();
+
+#endif
--- orchestrator.ino
+++ orchestrator.ino
@@ -1,54 +1,127 @@
-#include <Dhcp.h>
-#include <Dns.h>
-#include <Ethernet2.h>
-#include <EthernetClient.h>
-#include <EthernetServer.h>
-#include <EthernetUdp2.h>
-#include <Twitter.h>
-#include <util.h>
+#include "orchestrator.h"
 
-#include <SPI.h>
-
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192, 168, 1, 177);
-// Setup server with (port)
-EthernetServer server(80);
-
-int rcvdpayloadlen = 0;
-char rcvdpayload[255];
-byte nextbyte;
-
-void setup() {
-  // initialize the ethernet device
-  Ethernet.begin(mac, ip);
-  Serial.begin(9600);
-  // start listening for clients
-  server.begin();
-
-  while(!Serial){};
-  Serial.println(Ethernet.localIP());
+bool sanitizepayload(int nextbyte){
+  //check if number, uppercase or lowercase
+  return ((48 <= nextbyte & nextbyte <= 57 ) | (65 <= nextbyte & nextbyte <= 90) | (97 <= nextbyte & nextbyte <= 122));
 }
 
-boolean clientConnected = false;
-int ConnectionCheck = 0;
+void handlepayload(byte *payload, int payload_len){  
+  //String function = "";
+  char canbuffer[8];
+  bool canrcv = false;
+  bool cansnd = true;
+  bool transmitting = true;
+
+  //start test on MT
+  connectToMT();
+  /*
+  for(int i = 0; i < payload_len; i++){
+    function += (char)payload[i];
+  }
+  */
+  //execMTFunctionCall(function, strlen(function));
+  //start test on module
+  payload[payload_len + 1] = EOF;
+  sndCan(payload, 0x1, payload_len + 1);
+  while(transmitting){
+    while(!(len = rcvCan()));
+    for(int i = 0; i < len; i++){
+      if(rxBuf[i] == EOT){
+        transmitting = false;
+        break;
+      } else if(rxBuf[i] == WFA){
+        while(!clientMT.available()){};
+        sndCan(clientMT.read(), 1, 1);
+        break;
+      } else if(rxBuf[i] == SOR){
+        transmitting = false;
+        break;
+      } else{
+        clientMT.write(rxBuf[i]);  
+      }
+    }
+  }
+  
+  execMTFunctionCall("quit", sizeof("quit"));
+
+  while(resultCollecting){
+    while(!(len = rcvCan()));
+    for(int i = 0; i < len; i++){
+      if(rxBuf[i] == EOR){
+        resultCollecting = false;
+        break;
+      }
+      clientAdmin.write(rxBuf[i]);
+    }
+  }
+  
+  while(clientAdmin.connected()){
+    clientAdmin.flush();
+    clientAdmin.stop();
+  }
+}
+
+bool connectToMT(){
+  if (clientMT.connect(serverMT, 6340)) {
+    clientMT.write(1);
+    while(!clientMT.available()){}
+    if(clientMT.read()){
+      Serial.println("connected to server");
+      return true;
+    }
+  }
+  return false;
+}
+
+void disconnectFromMT(){
+    
+}
+
+bool execMTFunctionCall(char *functionCall, size_t len){
+    len -= 1;
+    clientMT.write(len);
+    
+    if(debug)
+      Serial.print("Functioncall size of  ");
+      Serial.print(functionCall);
+      Serial.print(": ");
+      Serial.print(len);
+      Serial.println();
+      
+    clientMT.print(functionCall);
+    while(!clientMT.available()){}
+    if(clientMT.read() == len){        
+      Serial.println("Successfull functionCall");
+      return true;
+    }
+    return false;
+}
 
 void loop() {
   while(!Serial);
   Serial.println("Waiting for answer");
-  EthernetClient client;
-  //waiting for someone to send
-  while(!client.connected()){
-    client = server.available();
-  }
-  while(!server.available() & client.connected());
-  if(!client.connected()) return;
+  EthernetClient clientAdmin;
+  /*
+  sndCan(msg, 18, 1);
+  while(!(len = rcvCan()));
+  */
   
-  nextbyte = client.read();
+  //waiting for someone to send
+  while(!clientAdmin.connected()){
+    clientAdmin = server.available();
+  }
+  while(!clientAdmin.available() & clientAdmin.connected());
+  if(!clientAdmin.connected()) return;
+  
+  nextbyte = clientAdmin.read();
   Serial.println(nextbyte);
-  client.write(nextbyte);
-  Serial.print("Nextbyte: ");
-  Serial.print(nextbyte);
-  Serial.println();
+  //clientAdmin.write(nextbyte);
+  
+  if(debug)
+    Serial.print("Nextbyte: ");
+    Serial.print(nextbyte);
+    Serial.println();
+    
   if(sanitizepayload(nextbyte)){
     rcvdpayload[rcvdpayloadlen] = nextbyte;    
     rcvdpayloadlen += 1;
@@ -63,47 +136,97 @@ void loop() {
       Serial.print(rcvdpayload[i]);
     }
     Serial.println();
-    //handlepayload(rcvdpayload, rcvdpayloadlen);
+    handlepayload(rcvdpayload, rcvdpayloadlen);
     rcvdpayloadlen = 0;
   } else{
     Serial.println("FAULTY PAYLOAD, DISCONNECTING...");
     //negative transmission code sent, faulty input
-    client.write(21);
-    while(client.connected()){
-      client.flush();
-      client.stop();
+    clientAdmin.write(21);
+    while(clientAdmin.connected()){
+      clientAdmin.flush();
+      clientAdmin.stop();
     }
   }
 
 }
 
-bool sanitizepayload(int nextbyte){
-  //check if number, uppercase or lowercase
-  return ((48 <= nextbyte & nextbyte <= 57 ) | (65 <= nextbyte & nextbyte <= 90) | (97 <= nextbyte & nextbyte <= 122));
+void setup() {
+  // initialize the ethernet device
+  Ethernet.begin(mac, ip);
+  Serial.begin(115200);  // CAN is running at 500,000BPS; 115,200BPS is SLOW, not FAST, thus 9600 is crippling.
+  // start listening for clients
+  server.begin();
+
+  while(!Serial){};
+  Serial.println(Ethernet.localIP());
+
+  canInit();
 }
 
-void handlepayload(byte payload, int payload_len){
-  #define EOT 4 //end of transmission
-  #define SOR 5 //start of result
-  #define EOR 6 //end of result
-  
-  String function = "";
-  char canbuffer[8];
-  bool canrcv = false;
-  bool cansnd = true;
-  
-}
+//#################### CAN #################################################
 
-String rcvFunc(){
-  while(cansnd){
-    for(int i = 0; i < 8; i++){
-      if(canbuffer[i] == EOF){
-        execMTFunctionCall(function, strlen(function));
-        function = "";
+int rcvCan(){
+  if(!digitalRead(CAN0_INT)){                          // If CAN0_INT pin is low, read receive buffer
+    CAN0.readMsgBuf(&rxId, &len, rxBuf);              // Read data: len = data length, buf = data byte(s)
+    /*
+    if((rxId & 0x80000000) == 0x80000000)             // Determine if ID is standard (11 bits) or extended (29 bits)
+      sprintf(msgString, "Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);
+    else
+      sprintf(msgString, "Standard ID: 0x%.3lX       DLC: %1d  Data:", rxId, len);
+    //Serial.print(len);
+    */
+    
+    if((rxId & 0x40000000) == 0x40000000){            // Determine if message is a remote request frame.
+      sprintf(msgString, " REMOTE REQUEST FRAME");
+      //Serial.print(msgString);
+    } else {
+      for(byte i = 0; i<len; i++){
+        msgString[i] = rxBuf[i];
       }
-    }
+    }    
+    return len;
   }
+  return 0;
 }
+
+bool sndCan(byte *msg, int msg_len, int dest_id){
+  byte canbuffer[8];
+  int i = 0;
+  int q = dest_id;
+  for(i = 0;i < msg_len; i++){
+    if(!(i%8) && i > 0){
+      if(CAN0.sendMsgBuf(q, 8, canbuffer) == CAN_OK)        
+        Serial.println("CAN - Message Sent Successfully!");
+      else
+        Serial.println("Error Sending CAN - Message...");
+      
+      q++;
+    }
+    canbuffer[i%8] = msg[i];
+  }
+  
+  canbuffer[msg_len % 8] = EOT;
+  if(CAN0.sendMsgBuf(q, (msg_len)%8 + 1, canbuffer) == CAN_OK)
+    Serial.println("CAN - Message Sent Successfully!");
+  else
+    Serial.println("Error Sending CAN - Message...");
+}
+
+void canInit(){
+  while(!Serial){};
+  // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
+  if(CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ) == CAN_OK)
+    Serial.println("MCP2515 Initialized Successfully!");
+  else
+    Serial.println("Error Initializing MCP2515...");
+  
+  // Since we do not set NORMAL mode, we are in loopback mode by default.
+  //CAN0.setMode(MCP_NORMAL);
+
+  pinMode(CAN0_INT, INPUT);                           // Configuring pin for /INT input
+}
+
+//#################### CAN #################################################
   /*    
   if(debug):
     Serial.print("PayloadLen: ");
