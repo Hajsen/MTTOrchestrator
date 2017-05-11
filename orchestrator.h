a4871a2d751d2eba7dc513a8ab5cccde8ccad4ad
Hajsen <Wilmok-2@student.ltu.se>
2017-05-11 09:31:07 +0400

    tests working ish

--- orchestrator.h
+++ orchestrator.h
@@ -1,11 +1,9 @@
-#include <Dhcp.h>
-#include <Dns.h>
-#include <Ethernet2.h>
+#include <Ethernet.h>
 #include <EthernetClient.h>
 #include <EthernetServer.h>
-#include <EthernetUdp2.h>
-#include <Twitter.h>
-#include <util.h>
+#include <EthernetUdp.h>
+
+
 
 #ifndef ORCHESTRATOR_H_
 #define ORCHESTRATOR_H_
@@ -22,50 +20,52 @@
 bool debug = 1;
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192, 168, 1, 177);
+IPAddress ip(10,160,238,14);
 IPAddress serverMT(10,160,238,15); 
 // Setup server with (port)
 EthernetServer server(80);
-EthernetClient clientAdmin;
+//EthernetClient clientAdmin;
 EthernetClient clientMT;
+
 
 //Ethernet
 int rcvdpayloadlen = 0;
 char rcvdpayload[255];
 byte nextbyte;
 
-boolean clientConnected = false;
-int ConnectionCheck = 0;
+//boolean clientConnected = false;
+//int ConnectionCheck = 0;
 
-bool sanitizepayload(int nextbyte);
-void handlepayload(byte *payload, int payload_len);
+bool sanitizepayload(byte nextbyte);
+//void handlepayload(byte *payload, int payload_len);
 bool connectToMT();
-void disconnectFromMT();
+//void disconnectFromMT();
 bool execMTFunctionCall(char *functionCall, size_t len);
 void testDI();
 void testDO();
 void test20mAO();
+void enableMTPower();
 
-#include <mcp_can.h>
-#include <mcp_can_dfs.h>
+//#include <mcp_can.h>
+//#include <mcp_can_dfs.h>
 
 // CAN0 INT and CS
-#define CAN0_INT 6                              // Set INT to pin 6
+//#define CAN0_INT 6                              // Set INT to pin 6
 
-MCP_CAN CAN0(12);                               // Set CS to pin 12
+//MCP_CAN CAN0(12);                               // Set CS to pin 12
 
 // CAN RX Variables
-long unsigned int rxId;
-unsigned char len;
-unsigned char rxBuf[8];
+//long unsigned int rxId;
+//unsigned char len;
+//unsigned char rxBuf[8];
 
 // Serial Output String Buffer
-char msgString[128];
-size_t msgString_len;
-byte msg[] = {0x01, 0x04, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0xF1, 0xF2, 0xF3};
+//char msgString[128];
+//size_t msgString_len;
+//byte msg[] = {0x01, 0x04, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0xF1, 0xF2, 0xF3};
 
-int rcvCan();
-bool sndCan(byte *msg, int msg_len, int dest_id);
-void canInit();
+//int rcvCan();
+//bool sndCan(byte *msg, int msg_len, int dest_id);
+//void canInit();
 
 #endif
--- orchestrator.ino
+++ orchestrator.ino
@@ -1,10 +1,10 @@
 #include "orchestrator.h"
 
-bool sanitizepayload(int nextbyte){
+bool sanitizepayload(byte nextbyte){
   //check if number, uppercase or lowercase
   return ((48 <= nextbyte & nextbyte <= 57 ) | (65 <= nextbyte & nextbyte <= 90) | (97 <= nextbyte & nextbyte <= 122));
 }
-
+/*
 void handlepayload(byte *payload, int payload_len){  
   //String function = "";
   char canbuffer[8];
@@ -18,7 +18,7 @@ void handlepayload(byte *payload, int payload_len){
   for(int i = 0; i < payload_len; i++){
     function += (char)payload[i];
   }
-  */
+  *//*
   //execMTFunctionCall(function, strlen(function));
   //start test on module
   payload[payload_len + 1] = EOF;
@@ -61,14 +61,23 @@ void handlepayload(byte *payload, int payload_len){
     clientAdmin.stop();
   }
 }
+*/
+
+void emptyRead(EthernetClient client){
+  Serial.println("Emptying buffert!");
+  while(client.read() != -1){}
+}
 
 bool connectToMT(){
   if (clientMT.connect(serverMT, 6340)) {
     clientMT.write(1);
     while(!clientMT.available()){}
-    if(clientMT.read()){
-      Serial.println("connected to server");
-      return true;
+    Serial.print("Connectioncheck returned: ");
+    int c;
+    c = clientMT.read();
+    Serial.println(c);
+    if((char)c == 1){
+      Serial.println(F("C server"));
     }
   }
   return false;
@@ -77,32 +86,59 @@ bool connectToMT(){
 bool execMTFunctionCall(char *functionCall, size_t len){
     len -= 1;
     clientMT.write(len);
-    
+    /*
     if(debug)
+      Serial.println();
       Serial.print("Functioncall size of  ");
       Serial.print(functionCall);
       Serial.print(": ");
       Serial.print(len);
       Serial.println();
-      
+    */
     clientMT.print(functionCall);
     while(!clientMT.available()){}
-    if(clientMT.read() == len){        
-      Serial.println("Successfull functionCall");
+    if((char)clientMT.read() == len){  
       return true;
     }
     return false;
 }
+
 void testDI(){
   //TESTDI START
-  char di_sw_on = "\45";
-  Serial.println("Press enter to continue to DI_SW_ON is HIGH");
+  byte di_sw_on = "\45";
+  byte di_24_fb = "\10";
+  Serial.println("DI_SW_ON is HIGH");
   execMTFunctionCall("setDO", sizeof("setDO"));
-  clientMT.write(di_sw_on);
-  clientMT.write("\1");
+  delay(200);
+  clientMT.write((byte)0x2D);
+  delay(200);
+  clientMT.write((byte)0x01);
   while(!clientMT.available());
-  Serial.println("DI_SW_ON status: ");
-  Serial.println(clientMT.read());
+  Serial.print("DI_SW_ON state: ");
+  byte lol;
+  lol = clientMT.read();
+  while(lol != 255){
+    if(sanitizepayload(lol)){
+      Serial.println((char)lol);   
+    }
+    lol = clientMT.read();
+  }
+
+  delay(300);
+  execMTFunctionCall("readA", sizeof("readA"));
+  delay(600);
+  clientMT.write((byte)0xA);
+  delay(300);
+  while(!clientMT.available());
+  Serial.print("DI_24_FB: ");
+  lol = clientMT.read();
+  while(lol != 255){
+    if(sanitizepayload(lol)){
+      Serial.print((char)lol);   
+    }
+    lol = clientMT.read();
+  }
+  /*
   Serial.println("Press enter to continue to DI_SW_ON is LOW");
   while(Serial.available() == 0){}
   Serial.read();
@@ -111,68 +147,103 @@ void testDI(){
   clientMT.write("\0");
   while(!clientMT.available());
   Serial.println("DI_SW_ON status: ");
-  Serial.print(clientMT.read());
-  while(Serial.available() == 0){}
-  Serial.read();
+  Serial.println(clientMT.read());
+  delay(300);
+  execMTFunctionCall("readA", sizeof("readA"));
+  delay(300);
+  clientMT.write(di_24_fb);
+  delay(300);
+  while(!clientMT.available());
+  Serial.println("DI_24_FB: ");
+  Serial.println(clientMT.read());
+  
+  */
   //TESTDI END
 }
 
 void testDO(){
   char do_fb_1 = "\8";
   for(int i = 0; i < 8; i++){
-    Serial.println("Testing pin: ");
+    Serial.println();
     Serial.print(i);
-    execMTFunctionCall("readA", sizeof("readA"));
+    execMTFunctionCall("readA", sizeof("readA") );
+    delay(400);
     clientMT.write(do_fb_1);
     while(!clientMT.available());
-    Serial.println("Result: ");
+    Serial.print(F(" : "));
     Serial.print(clientMT.read());
-    Serial.println("Press enter to go to next pin!");
-    while(Serial.available() == 0){}
-    Serial.read();
   }  
+  Serial.println();
 }
 
 void test20mAO(){
+  #define DIO32 32
   #define DIO33 33
   #define DIO26 26
-  #define DIO32 32
   #define DIO34 34
-  const char* DIO_array[4] = {"\32", "\26", "\33", "\34"};
-  for(int i = 0;i < 4; i++){
+  const byte DIO_array[4] = {0x20, 0x21, 0x1A, 0x22};
+  for(int i = 0;i < 50; i++){
+    i = 3;
     //open current output
     execMTFunctionCall("setDO", sizeof("setDO"));
+    delay(300);
     clientMT.write(DIO_array[i]);
-    clientMT.write("\1");
+    delay(300);
+    clientMT.write(byte(0x01));
     while(!clientMT.available());
-    Serial.println("Pin: ");
+    Serial.print("Pin: ");
     Serial.print(DIO_array[i]);
     Serial.print(" status: ");
-    Serial.println(clientMT.read());
+    byte lol;
+    lol = clientMT.read();
+    while(lol != 255){
+      if(sanitizepayload(lol)){
+        Serial.print((char)lol);   
+      }
+      lol = clientMT.read();
+    }    
+    Serial.println();
+    delay(15000);
     //close the current output
     execMTFunctionCall("setDO", sizeof("setDO"));
+    delay(300);
     clientMT.write(DIO_array[i]);
-    clientMT.write("\0");
-    Serial.println("Open next current output");
-    while(Serial.available() == 0){}
-    Serial.read();
+    delay(300);
+    clientMT.write((byte)0x00);
+    Serial.println();
+    Serial.print("Pin: ");
+    Serial.print(DIO_array[i]);
+    Serial.print(" status: ");    
+    while(!clientMT.available());
+    lol = clientMT.read();
+    while(lol != 255){
+      if(sanitizepayload(lol)){
+        Serial.print((char)lol);   
+      }
+      lol = clientMT.read();
+    }
+    Serial.println();
+    delay(400);
+    Serial.println("Openening next current output");
   }
 }
 
 void loop() {
-  connectToMT();
-  while(!Serial);
+  
+  /*
   testDO();
-  while(Serial.available() == 0){}
-  Serial.read();
   testDI();
+  delay(300);
   while(Serial.available() == 0){}
   Serial.read();
   test20mAO();
+  /*
   while(Serial.available() == 0){}
   Serial.read();
-  clientMT.write("quit", sizeof("quit");
-  delay(1000);
+  clientMT.write("quit", sizeof("quit"));
+  */
+  execMTFunctionCall("quit", sizeof("quit"));
+  while(true){};
 }
 
 void setup() {
@@ -180,26 +251,48 @@ void setup() {
   Ethernet.begin(mac, ip);
   Serial.begin(115200);  // CAN is running at 500,000BPS; 115,200BPS is SLOW, not FAST, thus 9600 is crippling.
   // start listening for clients
-  server.begin();
+  //server.begin();
 
   while(!Serial){};
   Serial.println(Ethernet.localIP());
+  //Serial.println(F("C MT"));
+  //canInit();
+  enableMTPower();
+}
 
-  canInit();
+void enableMTPower(){
+  
+  connectToMT();
+  Serial.println("IO_DCU_ON is HIGH");
+  execMTFunctionCall("setDO", sizeof("setDO"));
+  delay(200);
+  clientMT.write((byte)0x15);
+  delay(200);
+  clientMT.write((byte)0x01);
+  while(!clientMT.available());
+  Serial.print("IO_DCU_ON state: ");
+  byte lol;
+  lol = clientMT.read();
+  while(lol != 255){
+    if(sanitizepayload(lol)){
+      Serial.println((char)lol);   
+    }
+    lol = clientMT.read();
+  }
 }
 
 //#################### CAN #################################################
-
+/*
 int rcvCan(){
   if(!digitalRead(CAN0_INT)){                          // If CAN0_INT pin is low, read receive buffer
     CAN0.readMsgBuf(&rxId, &len, rxBuf);              // Read data: len = data length, buf = data byte(s)
-    /*
+    
     if((rxId & 0x80000000) == 0x80000000)             // Determine if ID is standard (11 bits) or extended (29 bits)
       sprintf(msgString, "Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);
     else
       sprintf(msgString, "Standard ID: 0x%.3lX       DLC: %1d  Data:", rxId, len);
     //Serial.print(len);
-    */
+    
     
     if((rxId & 0x40000000) == 0x40000000){            // Determine if message is a remote request frame.
       sprintf(msgString, " REMOTE REQUEST FRAME");
@@ -250,6 +343,7 @@ void canInit(){
 
   pinMode(CAN0_INT, INPUT);                           // Configuring pin for /INT input
 }
+*/
 
 //#################### CAN #################################################
   /*    
@@ -260,3 +354,4 @@ void canInit(){
     Serial.println("Payload:");
     Serial.println(payload);
   */  
+  
